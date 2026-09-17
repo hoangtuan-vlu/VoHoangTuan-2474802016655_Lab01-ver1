a3b7e61e8a9d04f936ffc5ddbe9f145faefe13bf
hoangtuan-vlu <tuanvo.04032006@gmail.com>
2026-09-17 16:34:04 +0700

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,11 @@
+function checkLogin(username, password) {
+    if (username === 'admin' && password === '123') {
+        return true;
+    }
+    return false;
+}
+
+// Xuất hàm để sử dụng cho Jest (nếu dùng môi trường Node.js)
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = checkLogin;
+}
